1ed328f1ef4dbcb3dd02af8c9378f8adfa87b970
Kurt <kurt.dietrich.85@gmail.com>
2021-04-05 19:35:20 -0400

    Fourth Commit - trying to filter by date

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,8 +1,6 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
@@ -11,20 +9,55 @@ console.log(data);
 
 // Use d3 to update each cell's text with
 // UFO report values (City, State, Country, Shape, Duration, Comments)
-data.forEach(function(UFOSighting) {
-    console.log(UFOSighting);
-    var row = tbody.append("tr");
-    Object.entries(UFOSighting).forEach(function([key, value]) {
-      console.log(key, value);
+// data.forEach(function(UFOSighting) {
+//     console.log(UFOSighting);
+//     var row = tbody.append("tr");
+//     Object.entries(UFOSighting).forEach(function([key, value]) {
+//       console.log(key, value);
       // Append a cell to the row for each value
       // in the UFO sighting report
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
+
+var DateData = d3.select("#datetime")
 
 
-// New UFO sighting to add to the table
+function populateData(userEntry) {
+    userEntry.forEach((UFOSighting) =>{
+        var row = tbody.append("tr");
+        Object.entries(UFOSighting.forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);  
+        });
+    })
+}
+
+function runEnter ()
+
+var filterDataDate = tableData
+
+if (inputValueDate) {
+    filteredDataDate = filteredDataDate.filter(tableData => tableData.datetime === inputValueDate
+}
+
+tbody.html("");
+
+if (filteredDataDate !== 0) {
+    populateData(filteredDataDate);
+}
+
+
+  // New UFO sighting to add to the table
+
+// data.forEach((UFOSighting)) => {
+//     var row = tbody.append("tr");
+//     Object.entries(UFOSighting).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
 
 
 // {
@@ -36,39 +69,3 @@ data.forEach(function(UFOSighting) {
 //     durationMinutes: "13 minutes",
 //     comments: "Three bright red lights witnessed floating stationary over San Diego New Years Day 2010"
 //   },
-
-// Use D3 to select the table
-// var table = d3.select("table");
-
-// Use d3 to create a bootstrap striped table
-// http://getbootstrap.com/docs/3.3/css/#tables-striped
-// table.attr("class", "table table-striped");
-
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-// Use D3 to select the table body
-var newSighting = ["1/1/2010", "matthews", "nc", "us", "cigar", "15 minutes", "large, bright yellow object hovered over squirrel lake park before disappearinhg at warp speed"];
-
-// Append one table row `tr` to the table body
-
-// data.(function(UFOSighting) {
-
-var row = tbody.append("tr");
-// Append one cell for the date
-row.append("td").text(newSignting[0]);
-// Append one cell for the city
-row.append("td").text(newSighting[1]);
-// Append one cell for the state
-row.append("td").text(newSignting[2]);
-// Append one cell for the country
-row.append("td").text(newSighting[3]);
-// Append one cell for the shape
-row.append("td").text(newSignting[4]);
-// Append one cell for the duration
-row.append("td").text(newSighting[5]);
-// Append one cell for the comments
-row.append("td").text(newSighting[6]);
-
-console.log("tr");
-
