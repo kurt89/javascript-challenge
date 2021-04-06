db614dbad14956304e151919389d1da7b8049573
Kurt <kurt.dietrich.85@gmail.com>
2021-04-05 22:32:53 -0400

    sixth commit - table rows not displaying with filter

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,3 +1,9 @@
+// Office hours was taken up by other students' questions.
+// I was unable to get enough time with the TA during office hours,
+// both before and after class, on Monday evening 4/5.
+// We agreed to meet (again) at the very beginning of office hours, 5:45pm,
+// on Wednesday, 4/7 so that I can then turn in a complete assignment.
+
 // from data.js
 var tableData = data;
 
@@ -6,34 +12,34 @@ var tableData = data;
 
 var DateData = d3.select("#datetime");
 
-// Console.log the weather data from data.js
-// console.log(data);
+// Console.log the UFO Sightings data from data.js
+console.log(data);
 
 // Use d3 to update each cell's text with
 // UFO report values (City, State, Country, Shape, Duration, Comments)
-// data.forEach(function(UFOSighting) {
-//     console.log(UFOSighting);
-//     var row = tbody.append("tr");
-//     Object.entries(UFOSighting).forEach(function([key, value]) {
-//       console.log(key, value);
+data.forEach(function(UFOSighting) {
+    console.log(UFOSighting);
+    var row = tbody.append("tr");
+    Object.entries(UFOSighting).forEach(function([key, value]) {
+      console.log(key, value);
       // Append a cell to the row for each value
       // in the UFO sighting report
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
 
 
-function populateData(userEntry) {
-    userEntry.forEach((UFOSighting) =>{
-        console.log(UFOSighting);
-        var row = tbody.append("tr");
-        Object.entries(UFOSighting.forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);  
-        });
-    })
-}
+// function populateData(userEntry) {
+//     userEntry.forEach((UFOSighting) =>{
+//         console.log(UFOSighting);
+//         var row = tbody.append("tr");
+//         Object.entries(UFOSighting.forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);  
+//         }))
+//     })
+// }
 
 function runEnter () {
 
@@ -52,6 +58,7 @@ function runEnter () {
 };
 
 
+
   // New UFO sighting to add to the table
 
 // data.forEach((UFOSighting)) => {
@@ -62,4 +69,4 @@ function runEnter () {
 //     });
 //   });
 
-
+populateData(data)
