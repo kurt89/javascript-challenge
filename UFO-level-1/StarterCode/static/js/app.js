2ad48db2cd9c13dadcd088f99c7d3a5b5f2db654
Kurt <kurt.dietrich.85@gmail.com>
2021-04-07 18:22:37 -0400

    eighth commit - HW complete 4/7 following Q&A with TA

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -2,65 +2,30 @@
 var tableData = data;
 
 // Get a reference to the table body
-// var tbody = d3.select("tbody");
-
+var tbody = d3.select("tbody");
 var DateData = d3.select("#datetime");
 
 // Console.log the UFO Sightings data from data.js
-console.log(data);
-
 // Use d3 to update each cell's text with
 // UFO report values (City, State, Country, Shape, Duration, Comments)
-data.forEach(function(UFOSighting) {
-    console.log(UFOSighting);
+function populateData(data) {
+  tbody.html('');
+  data.forEach(function (UFOSighting) {
     var row = tbody.append("tr");
-    Object.entries(UFOSighting).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the UFO sighting report
+    Object.entries(UFOSighting).forEach(function ([key, value]) {
       var cell = row.append("td");
       cell.text(value);
     });
   });
+}
 
-
-// function populateData(userEntry) {
-//     userEntry.forEach((UFOSighting) =>{
-//         console.log(UFOSighting);
-//         var row = tbody.append("tr");
-//         Object.entries(UFOSighting.forEach(([key, value]) => {
-//             var cell = row.append("td");
-//             cell.text(value);  
-//         }))
-//     })
-// }
-
-function runEnter () {
-
-    var filteredDataDate = tableData;
-    var inputValueDate = DateData.property("value");
-
-    if (inputValueDate) {
-        filteredDataDate = filteredDataDate.filter(tableData => tableData.datetime === inputValueDate)
-    };
-
-    tbody.html("");
-
-    if (filteredDataDate !== 0) {
-        populateData(filteredDataDate);
-    };
+function runEnter() {
+  var filteredData = tableData;
+  var inputValueDate = DateData.property("value");
+  if (inputValueDate) {
+    filteredData = filteredData.filter(obj => obj.datetime === inputValueDate)
+  };
+  populateData(filteredData);
 };
-
-
-
-  // New UFO sighting to add to the table
-
-// data.forEach((UFOSighting)) => {
-//     var row = tbody.append("tr");
-//     Object.entries(UFOSighting).forEach(([key, value]) => {
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
-
-populateData(data)
+populateData(data);
+d3.select('button').on('click', runEnter);
